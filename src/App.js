3b7bca86c0ded1e8b781f67d156079d93397c0a5
Wes Reid <bwreid@gmail.com>
2018-07-27 10:55:47 -0700

    Cleanup boilerplate

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="jumbotron jumbotron-fluid bg-light text-dark border-bottom">
+        <div className="container">
+          <h1 className="display-4">Color Mixer React App</h1>
+          <p className="lead">Install a JSX syntax highlighter package in your text editor to get started!</p>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
